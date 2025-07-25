2ea5e79f75703d649b28aec7fd9d1b8cfa685995
Voyager-3rd <quarts_wingman03@icloud.com>
2025-07-25 17:49:09 +0300

    in progress

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const inputParams = {
 	id: 6000,
 	order: 3000,
 	icon: "fa-car",
+	shift: "****",
 };
 
 const fs = require('fs');
@@ -14,8 +15,21 @@ const
 		"sr-RS",
 	];
 
-try {
+function getShift(row) {
+	let result = 0;
+	while (row.startsWith(inputParams.shift)) {
+		row = row.replace(inputParams.shift, "");
+		result++;
+	};
+	return result;
+};
 
+function getNextId(id) {
+	// TODO: need check for excluded values
+	return id + 1;
+};
+
+try {
 	const files = {};
 	fileNames.forEach(m => {
 		const text = fs.readFileSync(`./input/${m}.txt`, 'utf8');
@@ -53,7 +67,50 @@ try {
 		fs.writeFileSync(`./output/${m}.json`, i18nData[m]);
 	});
 
-	// console.log(i18nData);
+	const categories = {};
+	let id = inputParams.id;
+	const 
+		parentByShift = {},
+		orderByShift = {};
+
+	for (let index = 0; index < keyFileRows.length; index++) {
+		const 
+			row = keyFileRows[index],
+			shift = getShift(row),
+			name = i18nKeys[index];
+		
+		if (shift === 0) {
+			const item = {
+				name,
+				icon: inputParams.icon,
+				id,
+				parent: null,
+				children: [],
+				order: inputParams.order,
+			};
+
+			categories[id] = item;
+			parentByShift[shift] = item;
+		} else {
+			id = getNextId(id);
+			orderByShift[shift] = (orderByShift[shift] || 0) + 10;
+
+			const item = {
+				name,
+				id,
+				parent: parentByShift[shift - 1].id,
+				children: [],
+				order: orderByShift[shift],
+			};
+
+			categories[id] = item;
+			parentByShift[shift] = item;
+			parentByShift[shift - 1].children.push(id);
+		}
+		
+	};
+
+	fs.writeFileSync(`./output/categories.json`, JSON.stringify(categories, null, "\t"));
 
 } catch (error) {
 	console.error(error);
